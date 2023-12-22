a5e931cfec2c2b9518ebede12f6e280f41f97a59
Vladislav Pestsov <pestsov.js@gmail.com>
2023-12-22 10:57:29 +0200

    fix(server): wrap entry point with `initiate` and `terminate` processing methods

--- src/server/server.ts
+++ src/server/server.ts
@@ -4,4 +4,34 @@ import { ServerSymbols } from '@Server/Symbols';
 
 const initiator = container.get<IInitiator>(ServerSymbols.Initiator);
 
-initiator.start().catch((e) => console.log(e));
+const initiate = async () => {
+  await initiator.start();
+};
+
+const terminate = async () => {
+  await initiator.stop();
+  process.removeAllListeners();
+  process.exit(0);
+};
+
+process.on('SIGTERM', terminate);
+process.on('SIGINT', terminate);
+process.on('SIGHUP', terminate);
+process.on('uncaughtException', (e) => {
+  console.error(e);
+  initiator.stop().then(() => {
+    process.exit(1);
+  });
+});
+process.on('unhandledRejection', (reason, parameter) => {
+  parameter.catch((e) => {
+    console.error(e);
+    initiator.stop().then(() => {
+      process.exit(1);
+    });
+  });
+});
+
+initiate().catch((e) => {
+  console.log('Server end with error: ', e);
+});
