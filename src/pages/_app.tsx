ec9a90d171b02ffdd51c503d3bdc53de91954269
Vladislav Pestsov <pestsov.js@gmail.com>
2023-12-22 11:10:14 +0200

    chore(dependencies): install @nextui-org/react@2.2.9 | framer-motion@10.16.16

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,5 @@
 import './globals.css';
+import { NextUIProvider } from '@nextui-org/react';
 import { AppProps } from 'next/app';
 import { container } from '@EdgeContainer';
 import { EdgeSymbols } from '@EdgeSymbols';
@@ -8,5 +9,9 @@ import type { IInitiator } from '@Edge/Types';
 export default function MyApp(props: AppProps) {
   container.get<IInitiator>(EdgeSymbols.Initiator).start();
 
-  return <props.Component {...props.pageProps} />;
+  return (
+    <NextUIProvider>
+      <props.Component {...props.pageProps} />;
+    </NextUIProvider>
+  );
 }
